6e2b8f934784314f78d546a8a690e6972d8b9556
ArnavGoel77 <goelarnav06@gmail.com>
2026-09-05 21:28:39 +0530

    daily facts fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3u4s335u39o"
+    "revision": "0.ru5uk7qknd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
